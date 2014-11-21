6b217a242001845d667ece5cda24cd65f29fd7b7
valtovar <valtovar@gmail.com>
2014-11-21 01:05:04 -0500

    Draw Calls PoC and Optimization

---
+++ DrawCalls/Assets/TextureOptimizer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextureOptimizer : MonoBehaviour 
+{
+	public List<GameObject> gameObjects;
+	public Material refMaterial;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Material newMat = new Material (refMaterial);
+		Texture2D packedTexture = new Texture2D (1024, 1024);
+
+		Texture2D [] tmpTextures = new Texture2D[gameObjects.Count];
+		for ( int i = 0; i < gameObjects.Count; i++ )
+			tmpTextures[i] = gameObjects[i].renderer.material.mainTexture as Texture2D;
+
+		Rect [] uvs = packedTexture.PackTextures (tmpTextures, 0, 1024);
+		newMat.mainTexture = packedTexture;
+
+		Vector2[] uva, uvb;
+		for ( int i = 0; i < gameObjects.Count; i++ )
+		{
+			gameObjects[i].renderer.material = newMat;
+			uva = gameObjects[i].GetComponent<MeshFilter>().mesh.uv;
+			uvb = new Vector2[uva.Length];
+			for ( int j = 0; j < uva.Length; j++ )
+			{
+				uvb[j] = new Vector2 ( ( uva[j].x * uvs[i].width ) + uvs[i].x, ( uva[j].y * uvs[i].height ) + uvs[i].y );
+			}
+
+			gameObjects[i].GetComponent<MeshFilter>().mesh.uv = uvb;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ DrawCalls/Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Material t = new Material (renderer.sharedMaterial);
+		t.mainTextureOffset = new Vector2 (10, 10);
+		renderer.sharedMaterial = t;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
